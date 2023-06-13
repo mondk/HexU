130627ef6cb580d02c19a32e61b5d2de4bda6588
CBlixt <s176214@student.dtu.dk>
2023-06-13 09:53:37 +0200

    backToMenuOnline

--- src/OnlinePanel.java
+++ src/OnlinePanel.java
@@ -4,10 +4,12 @@ import java.awt.event.ActionEvent;
 import java.io.IOException;
 
 public class OnlinePanel extends JPanel {
+    private Panel panel;
     OnlinePanel(GameState gs){
         TextField ip = new TextField("127.0.0.1");
         JButton join = new JButton();
         JButton host = new JButton();
+        JButton backtoMenuOnline = new JButton();
         join.setAction(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
@@ -24,11 +26,19 @@ public class OnlinePanel extends JPanel {
                 gs.hostGame(ip.getText());
             }
         });
+        backtoMenuOnline.setAction(new AbstractAction() {
+            @Override
+			public void actionPerformed(ActionEvent e){
+				gs.returnToMenu();
+			}
+        });
         join.setText("Join game");
         host.setText("Host game");
+        backtoMenuOnline.setText("Back to Menu");
         //add(playerSettings.getPlayerCards());
         add(ip);
         add(join);
         add(host);
+        add(backtoMenuOnline);
     }
 }

30e5540be07d959c3ce18729e2c3ef4c5ce47c55
CBlixt <s176214@student.dtu.dk>
2023-06-16 11:45:23 +0200

    Commets... draeb mig

--- src/ColorButton.java
+++ src/ColorButton.java
@@ -3,19 +3,27 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+
+// Class to define ColorButton that displays a colored rectangle (the color can be set and changed).
 public class ColorButton extends JPanel {
     Color color = Color.red;
     JPanel cards;
 
     @Override
     protected void paintComponent(Graphics g) {
+
+        //Setting the color to the current color 
         g.setColor(color);
+
+        //Filling a rectangle with the current color depending on the coordinates and size 
         g.fillRect(0,0,50,50);
     }
-
+    // Constructor for the ColorButton class
     ColorButton(Color color, JPanel cards){
         this.cards = cards;
         this.color = color;
+
+        // Add a MouseListener to the ColorButton
         this.addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent mouseEvent) {
@@ -44,10 +52,11 @@ public class ColorButton extends JPanel {
             }
         });
     }
-
+    //Setting the color of the button
     void setColor(Color color){
         this.color = color;
     }
+    //Setting the cards for JPanel
     void setCards(JPanel cards){
         this.cards = cards;
     }

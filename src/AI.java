f119b59f88760cbbfcef3c55d5b9b340be09893a
mondk <66952082+mondk@users.noreply.github.com>
2023-06-18 11:16:33 +0200

    added nkip'
    w

--- src/AI.java
+++ src/AI.java
@@ -1,3 +1,4 @@
+//@authors Jasper and Laurits 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
--- src/GameState.java
+++ src/GameState.java
@@ -1,3 +1,4 @@
+//@author ALL
 import javax.swing.*;
 import java.awt.*;
 import java.util.*;
